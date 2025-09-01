3d3a7a1d8422f1bf255e6cc363564177b8fa228d
Wholesome100 <masteryuyda@gmail.com>
2025-08-31 21:01:54 -0400

    Set db context

---
+++ Data/FishDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using aspnet_fishies.Models;
+
+namespace aspnet_fishies.Data
+{
+    public class FishDbContext : DbContext
+    {
+        public FishDbContext(DbContextOptions<FishDbContext> options) : base(options) { }
+
+        public DbSet<Fish> Fish => Set<Fish>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Convert enums into strings for readability
+            modelBuilder.Entity<Fish>()
+                .Property(f => f.Habitat)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<Fish>()
+                .Property(f => f.Status)
+                .HasConversion<string>();
+        }
+    }
+}
