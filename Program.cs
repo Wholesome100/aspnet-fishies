daccf51256b67a421c29cd340d1414d33be15a45
Wholesome100 <masteryuyda@gmail.com>
2025-09-01 11:55:29 -0400

    Set POST and PUT endpoints

--- Program.cs
+++ Program.cs
@@ -53,6 +53,28 @@ app.MapGet("/fish", async (FishDbContext db) => await db.Fish.ToListAsync());
 app.MapGet("/fish/{id:int}", async (int id, FishDbContext db) =>
     await db.Fish.FindAsync(id) is Fish fish ? Results.Ok(fish) : Results.NotFound());
 
+app.MapPost("/fish", async (Fish fish, FishDbContext db) =>
+{
+    db.Fish.Add(fish);
+    await db.SaveChangesAsync();
+    return Results.Created($"/fish/{fish.Id}", fish);
+});
+
+app.MapPut("/fish/{id:int}", async (int id, Fish updated, FishDbContext db) =>
+{
+    var fish = await db.Fish.FindAsync(id);
+    if (fish is null) return Results.NotFound();
+
+    fish.Name = updated.Name;
+    fish.Habitat = updated.Habitat;
+    fish.Status = updated.Status;
+    fish.MarketValue = updated.MarketValue;
+
+    await db.SaveChangesAsync();
+    return Results.Ok(fish);
+});
+
+
 
 
 app.Run();
